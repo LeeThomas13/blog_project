39166fe09a63b2f5fd827549bfda3c0ec9cab886
LeeThomas13 <lmtthomasritz@gmail.com>
2020-12-16 19:52:47 -0800

    bug fixed some stuff, still not complete.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import BlogListView
+
+from .views import BlogListView, BlogDeleteView, BlogCreateView, BlogDetailView, BlogUpdateView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name='about page'),
+    path('', BlogListView.as_view(), name='home'),
+    path('delete/', BlogDeleteView.as_view(), name='delete page'),
+    path('create/', BlogCreateView.as_view(), name='create page'),
+    path('detail/', BlogDetailView.as_view(), name='detail page'),
+    path('update/', BlogUpdateView.as_view(), name='update page'),
 ]
